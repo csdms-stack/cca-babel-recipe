a8af696e46850d2c177c79a9d618307ddcaa4961
mcflugen <mcflugen@gmail.com>
2017-03-20 10:39:03 -0600

    Set MACOSX_DEPLOYMENT_TARGET to 10.9.

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,7 @@ export F90=$FC
 export F03=$FC
 
 if [ $(uname) == Darwin ]; then
+  export MACOSX_DEPLOYMENT_TARGET="10.9"
   export JAVAPREFIX=$(/usr/libexec/java_home)
 else
   # export JAVAPREFIX="${JAVA_HOME:-/usr/java/default}"
@@ -26,9 +27,15 @@ export FFLAGS="-Wl,-rpath,${PREFIX}/lib"
 ./configure --prefix=$PREFIX --disable-documentation \
   --with-libparsifal=$PREFIX --with-ltdl-include=$PREFIX/include \
   --with-ltdl-lib=$PREFIX/lib --with-libxml2=$PREFIX --with-chasm=$PREFIX \
-  --without-sidlx
+  --without-sidlx &> configure.txt
+cp configure.txt /Users/huttone/git/csdms-stack/cca-babel-recipe/recipe
 
-make all -j$CPU_COUNT > stdout.txt || (tail -100 stdout.txt && echo "ERROR in make all" && exit -1)
-make install > stdout.txt || (tail -100 stdout.txt && echo "ERROR in make install" && exit -1)
+# make all -j$CPU_COUNT > stdout.txt || (tail -100 stdout.txt && echo "ERROR in make all" && exit -1)
+# make install > stdout.txt || (tail -100 stdout.txt && echo "ERROR in make install" && exit -1)
+make all -j$CPU_COUNT &> make.all.txt
+cp make.all.txt /Users/huttone/git/csdms-stack/cca-babel-recipe/recipe
+
+make install &> make.install.txt
+cp make.install.txt /Users/huttone/git/csdms-stack/cca-babel-recipe/recipe
 
 rm "$PREFIX"/lib64 || echo "Unable to remove $PREFIX/lib64"
